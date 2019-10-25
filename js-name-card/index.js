de2daa9fadff8e592b9f7c4d36b7f212be1cb5f9
Jantzen-E <jantzen.egan@gmail.com>
2019-10-24 18:03:54 -0600

    json practice and js-name-card

---
+++ js-name-card/index.js
@@ -0,0 +1,36 @@
+const nameElement = document.querySelector('#name');
+const inputBoxElement = document.querySelector('#inputBox');
+const setNewNameButtonElement = document.querySelector('#setNewNameButton');
+
+inputBoxElement.addEventListener ('keypress', function(e) {
+    if(e.keyCode === 13) {
+        const newName = inputBoxElement.value;
+        // setNewNameFunction();
+        nameElement.textContent = newName;   
+    }
+});
+
+
+
+
+
+// const setNewNameFunction = function() {
+//     const name = inputBoxElement.value;
+
+//     nameElement.textContent = name;
+// }
+
+setNewNameButtonElement.addEventListener ('click', function() {
+    const newName = inputBoxElement.value;
+    nameElement.textContent = newName;
+
+});
+
+    
+
+
+
+// inputBoxElement.addEventListener('keypress', function(e) {
+//     if(e.keyCode === 13) {
+//         const item = inputBoxElement.value;
+
